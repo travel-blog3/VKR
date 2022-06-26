642c8e2fca45632f2f2db212f993cce578194cc0
travel-blog3 <73608072+travel-blog3@users.noreply.github.com>
2022-06-27 00:08:37 +0300

    Update face_module.py

--- face_module.py
+++ face_module.py
@@ -1,5 +1,6 @@
 import encodings 
-import os 
+import os
+import watchdog
 import cv2
 import face_recognition
 import numpy as np
@@ -7,6 +8,8 @@ from tqdm import tqdm
 from collections import defaultdict
 from imutils.video import VideoStream
 from eye_status import * 
+
+data_0 = []
 
 def start(): # загрузка путей к каскадам хаара
     face_cascPath = 'haarcascade_frontalface_alt.xml'
@@ -41,26 +44,17 @@ def process_and_encode(images):
     print("[LOG] Encoding dataset ...")
 
     for image_path in os.listdir(images):
-        image = cv2.imread('./images/' + image_path) # конвертирование в RGB
+        image = cv2.imread('./images/' + image_path)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
      
-        # detect face in the image and get its location (square boxes coordinates)
         boxes = face_recognition.face_locations(image, model='hog')
 
-        # Encode the face into a 128-d embeddings vector
         encoding = face_recognition.face_encodings(image, boxes)
-
-        # the person's name is the name of the folder where the image comes from
-        if image_path == "ales_17.jpg":
-            name = "aleksandr"
-        elif image_path == "pasha_18.jpg":
-            name = "pasha"
-        else:
-            name = image_path[:4]
 
         if len(encoding) > 0 : 
             known_encodings.append(encoding[0])
             known_names.append(name)
+     data_0 = known_encodings
 
         # print(known_encodings)
     return {"encodings": known_encodings, "names": known_names}
@@ -74,13 +68,11 @@ def isBlinking(history, maxFrames):
 
 def detect_and_display(model, video_capture, face_detector, open_eyes_detector, left_eye_detector, right_eye_detector, data, eyes_detected):
         frame = video_capture.read()
-        # resize the frame
         frame = cv2.resize(frame, (0, 0), fx=0.6, fy=0.6)
 
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         
-        # Detect faces
         faces = face_detector.detectMultiScale(
             gray,
             scaleFactor=1.2,
@@ -89,18 +81,13 @@ def detect_and_display(model, video_capture, face_detector, open_eyes_detector, 
             flags=cv2.CASCADE_SCALE_IMAGE
         )
 
-        # for each detected face
         for (x,y,w,h) in faces:
-            # Encode the face into a 128-d embeddings vector
             encoding = face_recognition.face_encodings(rgb, [(y, x+w, y+h, x)])[0]
 
-            # Compare the vector with all known faces encodings
             matches = face_recognition.compare_faces(data["encodings"], encoding)
 
-            # For now we don't know the person name
             name = "Unknown"
 
-            # If there is at least one match:
             if True in matches:
                 matchedIdxs = [i for (i, b) in enumerate(matches) if b]
                 counts = {}
@@ -108,7 +95,6 @@ def detect_and_display(model, video_capture, face_detector, open_eyes_detector, 
                     name = data["names"][i]
                     counts[name] = counts.get(name, 0) + 1
 
-                # determine the recognized face with the largest number of votes
                 name = max(counts, key=counts.get)
 
             face = frame[y:y+h,x:x+w]
@@ -116,8 +102,6 @@ def detect_and_display(model, video_capture, face_detector, open_eyes_detector, 
 
             eyes = []
             
-            # Eyes detection
-            # check first if eyes are open (with glasses taking into account)
             open_eyes_glasses = open_eyes_detector.detectMultiScale(
                 gray_face,
                 scaleFactor=1.1,
@@ -125,23 +109,18 @@ def detect_and_display(model, video_capture, face_detector, open_eyes_detector, 
                 minSize=(30, 30),
                 flags = cv2.CASCADE_SCALE_IMAGE
             )
-            # if open_eyes_glasses detect eyes then they are open 
             if len(open_eyes_glasses) == 2:
                 eyes_detected[name]+='1'
                 for (ex,ey,ew,eh) in open_eyes_glasses:
                     cv2.rectangle(face,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-            
-            # otherwise try detecting eyes using left and right_eye_detector
-            # which can detect open and closed eyes                
+                           
             else:
-                # separate the face into left and right sides
                 left_face = frame[y:y+h, x+int(w/2):x+w]
                 left_face_gray = gray[y:y+h, x+int(w/2):x+w]
 
                 right_face = frame[y:y+h, x:x+int(w/2)]
                 right_face_gray = gray[y:y+h, x:x+int(w/2)]
 
-                # Detect the left eye
                 left_eye = left_eye_detector.detectMultiScale(
                     left_face_gray,
                     scaleFactor=1.1,
@@ -149,8 +128,6 @@ def detect_and_display(model, video_capture, face_detector, open_eyes_detector, 
                     minSize=(30, 30),
                     flags = cv2.CASCADE_SCALE_IMAGE
                 )
-
-                # Detect the right eye
                 right_eye = right_eye_detector.detectMultiScale(
                     right_face_gray,
                     scaleFactor=1.1,
@@ -159,10 +136,8 @@ def detect_and_display(model, video_capture, face_detector, open_eyes_detector, 
                     flags = cv2.CASCADE_SCALE_IMAGE
                 )
 
-                eye_status = '1' # we suppose the eyes are open
+                eye_status = '1'
 
-                # For each eye check wether the eye is closed.
-                # If one is closed we conclude the eyes are closed
                 for (ex,ey,ew,eh) in right_eye:
                     color = (0,255,0)
                     pred = pred(right_face[ey:ey+eh,ex:ex+ew],model)
@@ -179,11 +154,8 @@ def detect_and_display(model, video_capture, face_detector, open_eyes_detector, 
                     cv2.rectangle(left_face,(ex,ey),(ex+ew,ey+eh),color,2)
                 eyes_detected[name] += eye_status
 
-            # Each time, we check if the person has blinked
-            # If yes, we display its name
             if isBlinking(eyes_detected[name],3):
                 cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-                # Display name
                 y = y - 15 if y - 15 > 15 else y + 15
                 cv2.putText(frame, name, (x, y), cv2.FONT_HERSHEY_SIMPLEX,0.75, (0, 255, 0), 2)
 
